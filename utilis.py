c8c659e6eabb7c61620a5c71aa0e4db9ad55d081
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 01:20:45 +0700

    Create utilis.py

---
+++ utilis.py
@@ -0,0 +1,14 @@
+st.markdown("""
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Inter', sans-serif;
+        }
+
+        section[data-testid="stSidebar"],
+        section[data-testid="stSidebar"] * {
+            color: #333 !important;
+            font-family: 'Inter', sans-serif !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
