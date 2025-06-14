58a76c1e514dfe87fb0253b0e3d192ae340c7cc7
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 01:27:42 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
 import streamlit as st
 
 def apply_global_styles():
-st.markdown("""
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    <style>
-        * {
-            font-family: 'Inter', sans-serif;
-        }
-        section[data-testid="stSidebar"],
-        section[data-testid="stSidebar"] * {
-            color: #333 !important;
-            font-family: 'Inter', sans-serif !important;
-        }
-    </style>
-""", unsafe_allow_html=True)
+    st.markdown("""
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+        <style>
+            * {
+                font-family: 'Inter', sans-serif;
+            }
+
+            /* Sidebar font and color */
+            section[data-testid="stSidebar"] *,
+            section[data-testid="stSidebar"] {
+                font-family: 'Inter', sans-serif !important;
+                color: #333 !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
