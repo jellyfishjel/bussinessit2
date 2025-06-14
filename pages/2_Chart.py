8c135b1e9f0c77bd941f7d414cda28a937d49032
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 02:15:14 +0700

    Update 2_Chart.py

--- pages/2_Chart.py
+++ pages/2_Chart.py
@@ -331,7 +331,7 @@ with graph_tab[1]:
                 spikesnap='cursor',
                 spikethickness=1.2,
                 spikedash='dot',
-                gridcolor='#333'
+                gridcolor='#555'
             ),
             legend=dict(orientation='h', yanchor='bottom', y=-0.3, xanchor='center', x=0.5)
         )
