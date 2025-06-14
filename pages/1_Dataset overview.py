fea5fb9b6fc97fb213f774b6b14be7ee3bd5d29d
jellyfishjel <meimeicute06@gmail.com>
2025-06-14 23:05:15 +0700

    Update 1_Dataset overview.py

--- pages/1_Dataset overview.py
+++ pages/1_Dataset overview.py
@@ -71,10 +71,12 @@ with tab2:
 
 # === TAB 3: VARIABLE EXPLANATION ===
 with tab3:
-    st.header("<p style="color: #cf5a2e"> 📊 Variable Explanation" </p>)
+    st.header("📊 Variable Explanation")
 
     # Section 1: Student Information
-    st.subheader("1. Student Information")
+    st.subheader("""
+        <h3 style='color: #cf5a2e; margin-top: 1rem; margin-bottom: 0.5rem;'>2. Academic Performance</h3>
+    """, unsafe_allow_html=True)
     st.markdown("""
     <div style='font-family: Inter, sans-serif; font-size: 15px; margin-bottom: 1rem;'>
         <ul>
