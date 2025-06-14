66f0efb5661b67c86c3a98a527534a32ba491256
jellyfishjel <meimeicute06@gmail.com>
2025-06-14 23:12:43 +0700

    Update 1_Dataset overview.py

--- pages/1_Dataset overview.py
+++ pages/1_Dataset overview.py
@@ -75,7 +75,7 @@ with tab3:
 
     # Section 1: Student Information
     st.markdown("""
-    <h3 style='font-family: Inter, sans-serif; font-size: 24px; margin-top: 1rem; margin-bottom: 0.5rem;'>1. Student Information</h3>
+    <h3 style='font-family: Inter, sans-serif; color: #cf5a2e; font-size: 24px; margin-top: 1rem; margin-bottom: 0.5rem;'>1. Student Information</h3>
     """, unsafe_allow_html=True)
     st.markdown("""
     <div style='font-family: Inter, sans-serif; font-size: 15px; margin-bottom: 1rem;'>
