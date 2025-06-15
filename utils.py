2fa32aee0611592e67cc7cbbe848d6be2cadef24
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 13:38:17 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,32 +40,3 @@ def apply_global_styles():
     """, unsafe_allow_html=True)
 
 
-def apply_chart_style(fig, title=None, xaxis_title=None, yaxis_title=None, legend_title=None):
-    fig.update_layout(
-        paper_bgcolor='rgba(0,0,0,0)',
-        plot_bgcolor='rgba(0,0,0,0)',
-        font=dict(color='#333'),
-        title=dict(text=title, font=dict(color='#333')) if title else None,
-        xaxis=dict(
-            title=xaxis_title if xaxis_title else None,
-            titlefont=dict(color='#333'),
-            tickfont=dict(color='#333')
-        ),
-        yaxis=dict(
-            title=yaxis_title if yaxis_title else None,
-            titlefont=dict(color='#333'),
-            tickfont=dict(color='#333')
-        ),
-        legend=dict(
-            title=dict(text=legend_title, font=dict(color='#333')) if legend_title else None,
-            orientation='h',
-            yanchor='bottom',
-            y=-0.3,
-            xanchor='center',
-            x=0.5,
-            font=dict(color='#333')
-        ),
-        margin=dict(t=40, l=40, r=40, b=60),
-        height=500
-    )
-    return fig
