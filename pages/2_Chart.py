e71e87197dd84e180d4e0e5ec06d0889b91e08fe
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 11:38:04 +0700

    Update 2_Chart.py

--- pages/2_Chart.py
+++ pages/2_Chart.py
@@ -94,7 +94,7 @@ graph_tab = st.tabs(["📈 Demographics", "📊 Job Offers"])
 # === TAB 1 (Demographics) ===
 with graph_tab[0]:
     st.markdown("""
-        <h1 style='font-family: "Inter", sans-serif; color: #cf5a2e; font-size: 36px;'>Demographics</h1>
+        <h1 style='font-family: "Inter", sans-serif; color: #cf5a2e; font-size: 40px;'>📊 Demographics</h1>
     """, unsafe_allow_html=True)
     
     chart_option = st.selectbox("Select Variable for Visualization", ['Gender Distribution', 'Field of Study'])
