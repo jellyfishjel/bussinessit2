454d38f18e6187a2edd5d9f0e4325b6f1e4908ae
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 12:06:58 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -32,7 +32,7 @@ def apply_global_styles():
             [data-testid="stMetricLabel"] {
                     color: #333 !important;
             }
-            label, span {
+            label, p {
                 color: #333 !important;
             }
         </style>
